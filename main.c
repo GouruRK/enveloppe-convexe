492613bf4214e73d7796357759577f04b1e8cd9e
qlabo <q.laborde1220@gmail.com>
2022-12-26 18:11:19 +0100

    transformation_struct_+_fonc

--- main.c
+++ main.c
@@ -42,6 +42,18 @@ int button_onclick(Button but, int x, int y) {
     return 1;
 }
 
+int button_onclick_tab(Button tab[], int size, int x, int y) {
+    for (int i = 0; i < size; i++) {
+        int but_max_x = tab[i].x + tab[i].width + BORDER;
+        int but_max_y = tab[i].y + tab[i].height + BORDER;
+        // est dans le bouton
+        if (!(x < tab[i].x || x > but_max_x || y < tab[i].y || y > but_max_y)) {
+            return i;
+        }
+    }
+    return -1;
+}
+
 Button button_create(int x, int y, char* text) {
     Button but;
     but.text = (char*)malloc(strlen(text) * sizeof(char));
@@ -57,12 +69,20 @@ Button button_create(int x, int y, char* text) {
 void button_draw_Wborder(Button but, MLV_Color color_text, MLV_Color color_border) {
     MLV_draw_rectangle(but.x, but.y, but.width + BORDER, but.height + BORDER, color_border);
     MLV_draw_text(but.x + BORDER / 2, but.y + BORDER / 2, but.text, color_text);
-    MLV_update_window();
 }
 
 void button_draw_WOborder(Button but, MLV_Color color_text) {
     MLV_draw_text(but.x + BORDER / 2, but.y + BORDER / 2, but.text, color_text);
-    MLV_update_window();
+}
+
+void switch_(int val[], int size, int index) {
+    for (int i = 0; i < size; i++) {
+        if (i == index) {
+            val[i] = 1;
+        } else {
+            val[i] = 0;
+        }
+    }
 }
 
 void window_param_preclose() {
@@ -75,23 +95,31 @@ void window_param_preclose() {
 
 void init_window_param() {
     int stop = 0, pressed = 0, x = 0, y = 0;
+    Button tabbut[3];
+    int tabval[3];
 
     MLV_execute_at_exit(exit_function, &stop);
     MLV_create_window("Setting convex hull", "Setting", 300, 300);
 
-    Button but;
-    but = button_create(100, 100, "Je m'appelle Michel");
+    tabbut[0] = button_create(100, 100, "Je m'appelle Michel");
+    tabbut[1] = button_create(100, 150, "Je m'appelle Michel");
+    tabbut[2] = button_create(100, 200, "Je m'appelle Michel");
 
     while (!stop) {
         MLV_clear_window(MLV_COLOR_BLACK);
         MLV_draw_text(150, 50, "%d", MLV_COLOR_GREEN, MLV_get_time());
-        button_draw_Wborder(but, MLV_COLOR_GREEN, MLV_COLOR_RED);
+        button_draw_Wborder(tabbut[0], MLV_COLOR_GREEN, MLV_COLOR_RED);
+        button_draw_WOborder(tabbut[1], MLV_COLOR_GREEN);
+        button_draw_WOborder(tabbut[2], MLV_COLOR_GREEN);
+        MLV_actualise_window();
         MLV_update_window();
 
         if ((MLV_get_mouse_button_state(MLV_BUTTON_LEFT) == MLV_PRESSED) && pressed == 0) {
             pressed = 1;
             MLV_get_mouse_position(&x, &y);
-            printf("%d\n", button_onclick(but, x, y));
+            int val = button_onclick_tab(tabbut, 3, x, y);
+            switch_(tabval, 3, val);
+            printf("%d %d %d\n", tabval[0], tabval[1], tabval[2]);
         }
         if (MLV_get_mouse_button_state(MLV_BUTTON_LEFT) == MLV_RELEASED) {
             pressed = 0;
