2d442df52f734539f7af3667989f26f0e9f0952b
qlabo <q.laborde1220@gmail.com>
2022-12-29 15:35:03 +0100

    error

--- utils/graphic/enveloppe.c
+++ utils/graphic/enveloppe.c
@@ -1,7 +1,15 @@
-#include "../utils.h"
 #include <MLV/MLV_all.h>
+
+#include "../utils.h"
 
 /*
 clang -c graphic.c -Wall -std=c17 -lMLV
+clang -c ./utils/list/list.c -Wall -std=c17
+clang -c ./utils/math/math.c list.o -Wall -std=c17
 clang enveloppe.c graphic.o -Wall -std=c17 -lMLV
 */
+
+int main(void) {
+    init_window_param();
+    return 0;
+}
